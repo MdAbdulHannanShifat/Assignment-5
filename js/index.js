6407a48fc56ff5c3d3a064b851eeb694de25388c
MdAbdulHannanShifat <abdulhannanshifat2003@gmail.com>
2024-09-25 09:09:52 +0600

    almost done

--- js/index.js
+++ js/index.js
@@ -12,6 +12,29 @@ const title = document.getElementById("title")
 const title2 = document.getElementById("title-2")
 const title3 = document.getElementById("title-3")
 
+const historySection = document.getElementById("history-section")
+const cardSection = document.getElementById("card-section")
+
+const dateTime = new Date().toLocaleString()
+
+donationButton.addEventListener("click",function(){
+   
+    cardSection.classList.remove("hidden")
+    historySection.classList.add("hidden")
+    // donationButton.classList.remove("bg-pColor")
+    historyButton.classList.remove("bg-pColor")
+    donationButton.classList.add("bg-pColor")
+})
+historyButton.addEventListener("click",function(){
+
+    historySection.classList.remove("hidden")
+    cardSection.classList.add("hidden")
+   historyButton.classList.add("bg-pColor")
+   donationButton.classList.remove("bg-pColor")
+   
+   })
+
+
 
 
 
@@ -37,6 +60,25 @@ donateBtn.addEventListener("click", function(){
     }
         document.getElementById("donate-box").innerHTML = donateBox + donateInput
      balance.innerText = updateBalance
+     const historyContent = document.getElementById("history-section");
+     
+     historyContent.innerHTML += `
+     
+     <div class="w-full border rounded-x1">
+     <div class="card-body">
+     <h2 class="text-lg">
+     <span>${donateInput}</span>Tk Donated For
+     
+     <span>${title.innerText}</span>
+     
+     </h2>
+     
+     <p class="p-4 bg-slate-100 rounded-xl">Date <span>${dateTime}
+     
+     </span></p>
+     </div>
+     </div>
+     `
     
     my_modal_5.showModal()
     document.getElementById("donate-input").value=""
@@ -66,6 +108,25 @@ donateBtn2.addEventListener("click", function(){
     }
         document.getElementById("donate-box-2").innerHTML = donateBox2 + donateInput2
      balance.innerText = updateBalance
+     const historyContent = document.getElementById("history-section");
+     
+     historyContent.innerHTML += `
+     
+     <div class="w-full border rounded-x1">
+     <div class="card-body">
+     <h2 class="text-lg">
+     <span>${donateInput2}</span>Tk Donated For
+     
+     <span>${title2.innerText}</span>
+     
+     </h2>
+     
+     <p class="p-4 bg-slate-100 rounded-xl">Date <span>${dateTime}
+     
+     </span></p>
+     </div>
+     </div>
+     `
     
     my_modal_5.showModal()
     document.getElementById("donate-input-2").value=""
@@ -96,10 +157,31 @@ donateBtn3.addEventListener("click", function(){
         document.getElementById("donate-box-3").innerHTML = donateBox3 + donateInput3
      balance.innerText = updateBalance
     
+     const historyContent = document.getElementById("history-section");
+     
+     historyContent.innerHTML += `
+     
+     <div class="w-full border rounded-x1">
+     <div class="card-body">
+     <h2 class="text-lg">
+     <span>${donateInput3}</span>Tk Donated For
+     
+     <span>${title3.innerText}</span>
+     
+     </h2>
+     
+     <p class="p-4 bg-slate-100 rounded-xl">Date <span>${dateTime}
+     
+     </span></p>
+     </div>
+     </div>
+     `
     my_modal_5.showModal()
     document.getElementById("donate-input-3").value=""
 
 })
+
+
 
 
 
